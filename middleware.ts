aae5de823cdb81b1134bd173cbe893a0dc93a0d1
jvras58 <jvras@cin.ufpe.br>
2025-07-28 00:18:46 -0300

    feat: atualizar redirecionamento de sessão para a rota de autenticação

--- middleware.ts
+++ middleware.ts
@@ -4,13 +4,13 @@ import { auth } from "./lib/auth";
 export async function middleware(request: NextRequest) {
   const session = await auth();
   if (!session || !session.user)
-    return NextResponse.redirect(new URL("/sign-in", request.url));
+    return NextResponse.redirect(new URL("/auth", request.url));
 
   return NextResponse.next();
 }
 
 export const config = {
   matcher: [
-    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|sign-in|sign-up|forgot-password|recovery-password|$).*)",
+    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|auth|map|forgot-password|recovery-password|$).*)",
   ],
 };
